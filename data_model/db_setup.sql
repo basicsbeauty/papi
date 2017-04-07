1cb2950dcc6ae3070f9f4550e6115ad60abb8319
BV <basicsbeauty@yahoo.com>
2017-04-07 13:08:48 -0700

    DB Cleanup

--- data_model/db_setup.sql
+++
@@ -1,9 +0,0 @@
-################################################################
-# Filename: parkapi/data_model/db_setup.sql
-# Location: /Users/Atom/repos/next/ridecell
-# Project :
-# Date    : 2017-04-07
-# Author  : Atom
-# Scope   :
-# Usage   :
-################################################################
